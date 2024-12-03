ee5de3a40d8b9735eb5b1b8dcb03aab4050aa387
Andrei Mackenzie <andreimackenzie@gmail.com>
2024-12-02 22:13:33 -0500

    2024-12-02 part 1

---
+++ 2024/2/go.mod
@@ -0,0 +1,3 @@
+module github.com/andrei-m/aoc/2024/2
+
+go 1.23.0
---
+++ 2024/2/red_nosed_reports.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	safeCount := 0
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		levels := strings.Fields(line)
+		safe, err := isSafe(levels)
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+		if safe {
+			safeCount++
+		}
+		log.Printf("%t: %v", safe, levels)
+	}
+	fmt.Printf("part 1: %d\n", safeCount)
+}
+
+func isSafe(levels []string) (bool, error) {
+	if len(levels) == 0 {
+		return true, nil
+	}
+	previous := mustGetInt(levels[0])
+	previousIncreasing := false
+
+	for i := 1; i < len(levels); i++ {
+		current := mustGetInt(levels[i])
+
+		diff := previous - current
+		absDiff := abs(diff)
+		if absDiff < 1 || absDiff > 3 {
+			log.Printf("absDiff index %d: %d", i, absDiff)
+			return false, nil
+		}
+		currentIncreasing := diff < 0
+
+		if i > 1 && (currentIncreasing != previousIncreasing) {
+			log.Printf("curentIncreasing: %t, previousIncreasting: %t", currentIncreasing, previousIncreasing)
+			return false, nil
+		}
+
+		previous = mustGetInt(levels[i])
+		previousIncreasing = currentIncreasing
+	}
+
+	return true, nil
+}
+
+func mustGetInt(raw string) int {
+	val, err := strconv.Atoi(raw)
+	if err != nil {
+		log.Fatalf("not an int %s: %v", raw, err)
+	}
+	return val
+}
+
+func abs(val int) int {
+	if val < 0 {
+		return val * -1
+	}
+	return val
+}
