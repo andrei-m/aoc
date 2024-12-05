2d37bebff4c67bfb2aae7fb5e22d5c1111ac802f
Andrei Mackenzie <andreimackenzie@gmail.com>
2024-12-04 20:36:29 -0500

    2024-12-03 part 2

--- 2024/3/corrupt.go
+++ 2024/3/corrupt.go
@@ -10,7 +10,7 @@ import (
 	"github.com/andrei-m/aoc/advent"
 )
 
-var pattern = regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+var pattern = regexp.MustCompile(`(.*?)?mul\((\d{1,3}),(\d{1,3})\)`)
 
 func main() {
 	buf, err := io.ReadAll(os.Stdin)
@@ -19,13 +19,54 @@ func main() {
 	}
 
 	submatches := pattern.FindAllStringSubmatch(string(buf), -1)
+
 	sum := 0
+	enabledSum := 0
+	enabled := true
+
 	for _, submatch := range submatches {
-		if len(submatch) != 3 {
-			log.Fatalf("unexpected match length != 3 for: %v", submatch)
+		if len(submatch) != 4 {
+			log.Fatalf("unexpected match length != 4 for: %v: %d", submatch, len(submatch))
 		}
-		sum += advent.MustParseInt(submatch[1]) * advent.MustParseInt(submatch[2])
+		newEnabled := toggleEnabled(submatch[1])
+		if newEnabled != nil {
+			enabled = *newEnabled
+		}
+		addition := advent.MustParseInt(submatch[2]) * advent.MustParseInt(submatch[3])
+		sum += addition
+		if enabled {
+			enabledSum += addition
+		}
+		log.Printf("%v enabled: %t, sum: %d, enabledSum: %d", submatch, enabled, sum, enabledSum)
 	}
 	fmt.Printf("part 1: %d\n", sum)
+	fmt.Printf("part 2: %d\n", enabledSum)
+}
 
+var (
+	do   = regexp.MustCompile(`do\(\)`)
+	dont = regexp.MustCompile(`don't\(\)`)
+)
+
+func toggleEnabled(prefix string) *bool {
+	var result bool
+	doMatches := do.FindAllIndex([]byte(prefix), -1)
+	dontMatches := dont.FindAllIndex([]byte(prefix), -1)
+
+	log.Printf("prefix: %s, doMatches: %v, dontMatches: %v", prefix, doMatches, dontMatches)
+
+	if len(doMatches) > 0 && len(dontMatches) > 0 {
+		lastDo := doMatches[len(doMatches)-1]
+		lastDont := dontMatches[len(dontMatches)-1]
+		result = lastDo[0] > lastDont[0]
+		return &result
+	} else if len(doMatches) > 0 {
+		result = true
+		return &result
+	} else if len(dontMatches) > 0 {
+		result = false
+		return &result
+	}
+
+	return nil
 }
