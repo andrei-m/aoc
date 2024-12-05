94494aca5d87da7f75f17ce17c359843ba8fba15
Andrei Mackenzie <andreimackenzie@gmail.com>
2024-12-04 22:06:01 -0500

    2024-12-04 part 2

--- 2024/4/xmas.go
+++ 2024/4/xmas.go
@@ -15,12 +15,17 @@ func main() {
 		puzzle = append(puzzle, strings.Split(scanner.Text(), ""))
 	}
 
+	solve1(puzzle)
+	solve2(puzzle)
+}
+
+func solve1(puzzle [][]string) {
 	matches := [][]int{}
 	count := 0
 	for y, row := range puzzle {
 		for col := range row {
 			if puzzle[y][col] == "X" {
-				newMatches, newCount := masCount(puzzle, y, col)
+				newMatches, newCount := masCount(puzzle, part1, y, col)
 				if newCount > 0 {
 					matches = append(matches, []int{y, col})
 					matches = append(matches, newMatches...)
@@ -49,6 +54,41 @@ func main() {
 	fmt.Printf("count: %d", count)
 }
 
+func solve2(puzzle [][]string) {
+	matches := [][]int{}
+	count := 0
+	for y, row := range puzzle {
+		for col := range row {
+			if puzzle[y][col] == "A" {
+				newMatches, newCount := masCount(puzzle, part2, y, col)
+				if newCount > 0 {
+					matches = append(matches, []int{y, col})
+					matches = append(matches, newMatches...)
+					count += newCount
+				}
+			}
+		}
+	}
+
+	debug := make([][]string, len(puzzle))
+	for i := range puzzle {
+		debug[i] = make([]string, len(puzzle[i]))
+
+		for j := range puzzle[i] {
+			debug[i][j] = "."
+		}
+	}
+	for _, match := range matches {
+		debug[match[0]][match[1]] = puzzle[match[0]][match[1]]
+	}
+
+	for _, row := range debug {
+		fmt.Println(strings.Join(row, ""))
+	}
+
+	fmt.Printf("count: %d\n", count)
+}
+
 type vector struct {
 	rowOffset int
 	colOffset int
@@ -59,58 +99,91 @@ type vector struct {
 type masMatch []vector
 
 // each of the opportunities countes as one 'MAS' count
-var opportunities = []masMatch{
-	{
-		// right
-		{1, 0, "M"},
-		{2, 0, "A"},
-		{3, 0, "S"},
-	},
-	{
-		// left
-		{-1, 0, "M"},
-		{-2, 0, "A"},
-		{-3, 0, "S"},
-	},
-	{
-		// down
-		{0, 1, "M"},
-		{0, 2, "A"},
-		{0, 3, "S"},
-	},
-	{
-		// up
-		{0, -1, "M"},
-		{0, -2, "A"},
-		{0, -3, "S"},
-	},
-	{
-		// down/right
-		{1, 1, "M"},
-		{2, 2, "A"},
-		{3, 3, "S"},
-	},
-	{
-		// up/left
-		{-1, -1, "M"},
-		{-2, -2, "A"},
-		{-3, -3, "S"},
-	},
-	{
-		// down/left
-		{1, -1, "M"},
-		{2, -2, "A"},
-		{3, -3, "S"},
-	},
-	{
-		// up/right
-		{-1, 1, "M"},
-		{-2, 2, "A"},
-		{-3, 3, "S"},
-	},
-}
+var (
+	part1 = []masMatch{
+		{
+			// right
+			{1, 0, "M"},
+			{2, 0, "A"},
+			{3, 0, "S"},
+		},
+		{
+			// left
+			{-1, 0, "M"},
+			{-2, 0, "A"},
+			{-3, 0, "S"},
+		},
+		{
+			// down
+			{0, 1, "M"},
+			{0, 2, "A"},
+			{0, 3, "S"},
+		},
+		{
+			// up
+			{0, -1, "M"},
+			{0, -2, "A"},
+			{0, -3, "S"},
+		},
+		{
+			// down/right
+			{1, 1, "M"},
+			{2, 2, "A"},
+			{3, 3, "S"},
+		},
+		{
+			// up/left
+			{-1, -1, "M"},
+			{-2, -2, "A"},
+			{-3, -3, "S"},
+		},
+		{
+			// down/left
+			{1, -1, "M"},
+			{2, -2, "A"},
+			{3, -3, "S"},
+		},
+		{
+			// up/right
+			{-1, 1, "M"},
+			{-2, 2, "A"},
+			{-3, 3, "S"},
+		},
+	}
 
-func masCount(puzzle [][]string, row int, col int) ([][]int, int) {
+	part2 = []masMatch{
+		// MAS / MAS
+		{
+			{-1, -1, "M"},
+			{1, 1, "S"},
+			{-1, 1, "M"},
+			{1, -1, "S"},
+		},
+		// SAM / MAS
+		{
+			{-1, -1, "S"},
+			{1, 1, "M"},
+			{-1, 1, "M"},
+			{1, -1, "S"},
+		},
+		// SAM / SAM
+		{
+			{-1, -1, "S"},
+			{1, 1, "M"},
+			{-1, 1, "S"},
+			{1, -1, "M"},
+		},
+		// MAS / SAM
+		{
+			{-1, -1, "M"},
+			{1, 1, "S"},
+			{-1, 1, "S"},
+			{1, -1, "M"},
+		},
+	}
+)
+
+func masCount(puzzle [][]string, opportunities []masMatch, row int, col int) ([][]int, int) {
 	matches := [][]int{}
 	count := 0
 	for _, opt := range opportunities {
