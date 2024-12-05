1b9dbcc9beeb09bc1875aaa269de624bfa7e7c12
Andrei Mackenzie <andreimackenzie@gmail.com>
2024-12-04 21:12:36 -0500

    initial stab at 2024-12-04

---
+++ 2024/4/xmas.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	puzzle := [][]string{}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		puzzle = append(puzzle, strings.Split(scanner.Text(), ""))
+	}
+
+	count := 0
+	for row := range puzzle {
+		for col := range row {
+			if puzzle[row][col] == "X" {
+				count += masCount(puzzle, row, col)
+			}
+		}
+	}
+
+	fmt.Printf("part 1: %d", count)
+}
+
+type vector struct {
+	rowOffset int
+	colOffset int
+	char      string
+}
+
+// all vectors in a 'MAS' match need to hit for the match to be considered
+type masMatch []vector
+
+// each of the opportunities countes as one 'MAS' count
+var opportunities = []masMatch{
+	{
+		{1, 0, "M"},
+		{2, 0, "A"},
+		{3, 0, "S"},
+	},
+	{
+		{-1, 0, "M"},
+		{-2, 0, "A"},
+		{-3, 0, "S"},
+	},
+	{
+		{0, 1, "M"},
+		{0, 2, "A"},
+		{0, 3, "S"},
+	},
+	{
+		{0, -1, "M"},
+		{0, -2, "A"},
+		{0, -3, "S"},
+	},
+	{
+		{1, 1, "M"},
+		{2, 2, "A"},
+		{3, 3, "S"},
+	},
+	{
+		{-1, -1, "M"},
+		{-2, -2, "A"},
+		{-3, -3, "S"},
+	},
+	{
+		{1, -1, "M"},
+		{2, -2, "A"},
+		{3, -3, "S"},
+	},
+	{
+		{1, -1, "M"},
+		{2, -2, "A"},
+		{3, -3, "S"},
+	},
+}
+
+func masCount(puzzle [][]string, row int, col int) int {
+	count := 0
+	for _, opt := range opportunities {
+		eligible := true
+
+		for _, v := range opt {
+			x := row + v.rowOffset
+			y := col + v.colOffset
+
+			if x < 0 || x > len(puzzle)-1 {
+				eligible = false
+				// row out of bounds
+				break
+			}
+			if y < 0 || y > len(puzzle[x])-1 {
+				eligible = false
+				// column out of bounds
+				break
+			}
+
+			if puzzle[x][y] != v.char {
+				eligible = false
+				// no character match
+				break
+			}
+		}
+
+		if eligible {
+			count++
+		}
+	}
+
+	return count
+}
