976577713dfaa1ecda54f4d67f82e9b2181f8b44
Andrei Mackenzie <andreimackenzie@gmail.com>
2024-12-07 10:53:54 -0500

    2024-12-05 part 2

--- 2024/5/launch_rules.go
+++ 2024/5/launch_rules.go
@@ -45,28 +45,48 @@ func main() {
 
 	sum := 0
 	for _, seq := range sequences {
-		if isOrdered(seq, ruleIndex) {
+		ordered, _, _ := isOrdered(seq, ruleIndex)
+		if ordered {
 			sum += seq[len(seq)/2]
 		}
 	}
-	fmt.Printf("sum: %d", sum)
+	fmt.Printf("part 1 sum: %d", sum)
+
+	sum = 0
+	for _, seq := range sequences {
+		firstTry := true
+		for {
+			ordered, i, j := isOrdered(seq, ruleIndex)
+			if ordered && firstTry {
+				// already in-order, so don't add to sum
+				break
+			} else if ordered {
+				sum += seq[len(seq)/2]
+				break
+			}
+			seq[i], seq[j] = seq[j], seq[i]
+			firstTry = false
+		}
+	}
+	fmt.Printf("part 2 sum: %d", sum)
 }
 
 // isOrdered returns true if each pair of (element in seq, each proceeding element in seq) does not violate the rules represented in ruleIndex
 // ruleIndex is keyed by the "last" element, e.g. the rule 46 -> [91, 22] means 22 and 91 must both preceed 46
-func isOrdered(seq []int, ruleIndex map[int][]int) bool {
+// isOrdered returns the two indices that violate the first encountered violated rule, or -1,-1 if no rules are violated
+func isOrdered(seq []int, ruleIndex map[int][]int) (bool, int, int) {
 	for i := range seq {
 		for j := 0; j < i; j++ {
 			requiredPredecessors := ruleIndex[seq[j]]
 			if advent.IntSliceContains(requiredPredecessors, seq[i]) {
 				log.Printf("FALSE: %v; %d|%d; rule %d -> %v", seq, seq[i], seq[j], seq[j], ruleIndex[seq[j]])
-				return false
+				return false, i, j
 			}
 		}
 	}
 
 	log.Printf("TRUE: %v", seq)
-	return true
+	return true, -1, -1
 }
 
 type rulePair struct {
