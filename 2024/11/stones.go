78856e8488898a460d7783ec9f43a3668d872159
Andrei Mackenzie <andreimackenzie@gmail.com>
2024-12-22 21:04:32 -0500

    2024-12-11 part 2
    The part 2 solution is a more space-efficient version that also works
    for part 1.

--- 2024/11/stones.go
+++ 2024/11/stones.go
@@ -18,13 +18,33 @@ func main() {
 	}
 
 	elements := strings.Split(scanner.Text(), " ")
+	orig := make([]string, len(elements))
+	copy(orig, elements)
 
 	for i := 0; i < 25; i++ {
-		log.Printf("%v", elements)
+		//log.Printf("%v", elements)
 		elements = evolve(elements)
-		//time.Sleep(5 * time.Second)
 	}
 	fmt.Printf("part 1: %d\n", len(elements))
+
+	elementsMap := make(map[int]int, len(orig))
+	for _, elem := range orig {
+		elementsMap[advent.MustParseInt(elem)] = 1
+	}
+	for i := 0; i < 75; i++ {
+		log.Printf("iteration %d: len(elements): %d", i+1, score(elementsMap))
+		elementsMap = evolvePart2(elementsMap)
+	}
+
+	fmt.Printf("part 2: %d\n", score(elementsMap))
+}
+
+func score(elements map[int]int) int {
+	sum := 0
+	for _, v := range elements {
+		sum += v
+	}
+	return sum
 }
 
 func evolve(elements []string) []string {
@@ -41,6 +61,22 @@ func evolve(elements []string) []string {
 	return next
 }
 
+func evolvePart2(elements map[int]int) map[int]int {
+	next := map[int]int{}
+	for k := range elements {
+		if k == 0 {
+			next[1] += elements[k]
+		} else if evenDigits(k) {
+			split := splitInt(k)
+			next[split[0]] += elements[k]
+			next[split[1]] += elements[k]
+		} else {
+			next[k*2024] += elements[k]
+		}
+	}
+	return next
+}
+
 func split(elem string) []string {
 	first := elem[:len(elem)/2]
 	last := elem[len(elem)/2:]
@@ -51,4 +87,33 @@ func split(elem string) []string {
 
 func mult(elem string) string {
 	return strconv.Itoa(advent.MustParseInt(elem) * 2024)
+}
+
+var evenDigitsMemo = map[int]bool{}
+
+func evenDigits(elem int) bool {
+	even, ok := evenDigitsMemo[elem]
+	if ok {
+		return even
+	}
+	str := strconv.Itoa(elem)
+	even = len(str)%2 == 0
+	evenDigitsMemo[elem] = even
+	return even
+}
+
+var splitIntMemo = map[int][]int{}
+
+func splitInt(elem int) []int {
+	split, ok := splitIntMemo[elem]
+	if ok {
+		return split
+	}
+	str := strconv.Itoa(elem)
+	first := str[:len(str)/2]
+	last := str[len(str)/2:]
+	firstInt := advent.MustParseInt(first)
+	lastInt := advent.MustParseInt(last)
+	splitIntMemo[elem] = []int{firstInt, lastInt}
+	return splitIntMemo[elem]
 }
