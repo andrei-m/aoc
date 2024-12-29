649282950d4eb09e453ee6124451615e528641c0
Andrei Mackenzie <andreimackenzie@gmail.com>
2024-12-28 20:09:53 -0500

    2024-12-13 part 1

--- 2024/13/claw.go
+++ 2024/13/claw.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"regexp"
+	"sort"
 
 	"github.com/andrei-m/aoc/advent"
 )
@@ -15,8 +17,46 @@ func main() {
 		Look for (a, b) pairs that satisfy both axes. Iterate between 0 and 100 for each potential value.
 		Sort the satisfying pairs by token cost (a==3 tokens; b==1 token), ascending. Select the first pair, which is most efficient. Add its token cost to the sum.
 	*/
-	scenarios := mustParseScenarios
-	log.Printf("parsed %d scenarios", len(scenarios()))
+	scenarios := mustParseScenarios()
+	log.Printf("parsed %d scenarios", len(scenarios))
+
+	var tokens int
+	for _, s := range scenarios {
+		sols := getSolutions(s)
+		if len(sols) > 0 {
+			sort.Slice(sols, func(i, j int) bool {
+				return sols[i].score() < sols[j].score()
+			})
+			log.Printf("scenario: %v; solutions: %v: best score: %d", s, sols, sols[0].score())
+			tokens += sols[0].score()
+		} else {
+			log.Printf("scenario: %v; no solution", s)
+		}
+	}
+	fmt.Printf("part 1: %d\n", tokens)
+}
+
+type solution struct {
+	a int
+	b int
+}
+
+func (s solution) score() int {
+	return s.a*3 + s.b
+}
+
+func getSolutions(scen scenario) []solution {
+	solutions := []solution{}
+
+	for i := 0; i <= 100; i++ {
+		for j := 0; j <= 100; j++ {
+			if i*scen.aDelta.X+j*scen.bDelta.X == scen.prizeLoc.X && i*scen.aDelta.Y+j*scen.bDelta.Y == scen.prizeLoc.Y {
+				solutions = append(solutions, solution{a: i, b: j})
+			}
+		}
+	}
+
+	return solutions
 }
 
 type scenario struct {
@@ -34,7 +74,7 @@ func mustParseScenarios() []scenario {
 			break
 		}
 		scen := mustParseScenario(lines)
-		log.Printf("scenario: %v", scen)
+		//log.Printf("scenario: %v", scen)
 		scenarios = append(scenarios, scen)
 
 		// two newlines between scenarios, except for the last
