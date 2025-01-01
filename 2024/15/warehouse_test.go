6e3899159607966301d6955ad530e792b77f4a5a
Andrei Mackenzie <andreimackenzie@gmail.com>
2025-01-01 13:31:05 -0500

    2024-12-15 Verify the part 2 'score' logic

--- 2024/15/warehouse_test.go
+++ 2024/15/warehouse_test.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
 /*
 func Test_debugPart2(t *testing.T) {
 
@@ -11,3 +17,19 @@ func Test_debugPart2(t *testing.T) {
 	part2(m, dirs)
 }
 */
+
+func Test_score(t *testing.T) {
+	m := [][]object{
+		{wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+		{wall, wall, boxLeft, boxRight, empty, empty, empty, empty, empty, empty, empty, boxLeft, boxRight, empty, boxLeft, boxRight, boxLeft, boxRight, wall, wall},
+		{wall, wall, boxLeft, boxRight, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, boxLeft, boxRight, empty, wall, wall},
+		{wall, wall, boxLeft, boxRight, empty, empty, empty, empty, empty, empty, empty, empty, boxLeft, boxRight, boxLeft, boxRight, boxLeft, boxRight, wall, wall},
+		{wall, wall, boxLeft, boxRight, empty, empty, empty, empty, empty, empty, boxLeft, boxRight, empty, empty, empty, empty, boxLeft, boxRight, wall, wall},
+		{wall, wall, empty, empty, wall, wall, empty, empty, empty, empty, empty, empty, boxLeft, boxRight, empty, empty, empty, empty, wall, wall},
+		{wall, wall, empty, empty, boxLeft, boxRight, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, wall, wall},
+		{wall, wall, empty, empty, robot, empty, empty, empty, empty, empty, empty, boxLeft, boxRight, empty, boxLeft, boxRight, boxLeft, boxRight, wall, wall},
+		{wall, wall, empty, empty, empty, empty, empty, empty, boxLeft, boxRight, boxLeft, boxRight, empty, empty, boxLeft, boxRight, empty, empty, wall, wall},
+		{wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall},
+	}
+	assert.Equal(t, 9021, score(m))
+}
