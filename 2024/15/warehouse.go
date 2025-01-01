5e0638c8eb197740ad143747265aa88753c43a86
Andrei Mackenzie <andreimackenzie@gmail.com>
2025-01-01 12:06:28 -0500

    2024-12-15 - part 2 work in progress
    Boxes move as they should, but the score is too high.

--- 2024/15/warehouse.go
+++ 2024/15/warehouse.go
@@ -3,15 +3,17 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/andrei-m/aoc/advent"
 )
 
 func main() {
-	m, dirs := mustParseInput()
+	m, dirs := mustParseInput(os.Stdin)
 	originalM := make([][]object, len(m))
 	for y := range m {
 		row := make([]object, len(m[y]))
@@ -87,15 +89,65 @@ func main() {
 			- both adjacent spaces are empty or contain movable boxes
 		left and right halves of boxes must move together
 	*/
+	part2(originalM, dirs)
+}
 
-	log.Printf("%v", originalM)
-	mPart2 := getPart2Map(originalM)
-	drawMap(originalM)
-	drawMap(mPart2)
+func part2(m [][]object, dirs []advent.Direction) {
+	mPart2 := getPart2Map(m)
+	adjacentDir = advent.AdjacentDirFn(len(mPart2[0]), len(mPart2))
+
+	var robotPos advent.Point
+	for y := range mPart2 {
+		for x := range mPart2[y] {
+			if mPart2[y][x] == robot {
+				robotPos = advent.Point{X: x, Y: y}
+			}
+		}
+	}
+
+	for i, dir := range dirs {
+		var nextMoveSuffix string
+		if i < len(dirs)-1 {
+			nextMoveSuffix = fmt.Sprintf("; next move: %v", dirs[i+1])
+		}
+		boxesMoved := 0
+		for {
+			vacatedPos, newPos := maybeMove(mPart2, robotPos, dir)
+			if vacatedPos == nil {
+				drawMap(mPart2)
+				fmt.Printf("(%d,%d) direction #%d: cannot move %s%s\n", robotPos.X, robotPos.Y, i+1, dir, nextMoveSuffix)
+				time.Sleep(1 * time.Second)
+				// can't move in direction
+				break
+			}
+			if *vacatedPos == robotPos {
+				// robot moved
+				robotPos = *newPos
+				drawMap(mPart2)
+
+				boxMoveSuffix := ""
+				if boxesMoved == 1 {
+					boxMoveSuffix = " (moved 1 box)"
+				} else if boxesMoved > 1 {
+					boxMoveSuffix = fmt.Sprintf(" (moved %d boxes)", boxesMoved)
+				}
+				fmt.Printf("(%d,%d) direction #%d: moved %s%s%s\n", robotPos.X, robotPos.Y, i+1, dir, boxMoveSuffix, nextMoveSuffix)
+
+				time.Sleep(1 * time.Second)
+				break
+			}
+			// moved a box; try moving again
+			boxesMoved++
+		}
+	}
+
+	fmt.Printf("part 2: %d\n", score(mPart2))
 }
 
 var adjacentDir func(advent.Point, advent.Direction) *advent.Point
 
+// maybe move returns the vacated and new position of the moved object, or nil if no move occured
+// For 2x1 boxes, the left half is represented in returned Points (both halves move)
 func maybeMove(m [][]object, pos advent.Point, dir advent.Direction) (*advent.Point, *advent.Point) {
 	adj := adjacentDir(pos, dir)
 	if adj == nil {
@@ -108,11 +160,84 @@ func maybeMove(m [][]object, pos advent.Point, dir advent.Direction) (*advent.Po
 		return &pos, adj
 	case wall:
 		return nil, nil
-	case box:
-		return maybeMove(m, *adj, dir)
+	case box, boxLeft, boxRight:
+		return maybeMoveBox(m, *adj, dir)
 	default:
 		log.Fatalf("unexpected object at position (%d,%d)", adj.X, adj.Y)
 	}
+	return nil, nil
+}
+
+func maybeMoveBox(m [][]object, pos advent.Point, dir advent.Direction) (*advent.Point, *advent.Point) {
+	if m[pos.Y][pos.X] == box {
+		// 1x1 boxes use the same movement rules as robots
+		return maybeMove(m, pos, dir)
+	}
+
+	var posLeft, posRight advent.Point
+	if m[pos.Y][pos.X] == boxLeft {
+		posLeft = pos
+		posRight = *adjacentDir(pos, advent.Right)
+	} else if m[pos.Y][pos.X] == boxRight {
+		posLeft = *adjacentDir(pos, advent.Left)
+		posRight = pos
+	} else {
+		log.Fatalf("(%d,%d) does not contain a box", pos.X, pos.Y)
+	}
+
+	adjLeft := adjacentDir(posLeft, dir)
+	adjRight := adjacentDir(posRight, dir)
+	switch dir {
+	case advent.Down, advent.Up:
+		if adjLeft == nil || adjRight == nil {
+			// reached a map edge
+			return nil, nil
+		}
+		if m[adjLeft.Y][adjLeft.X] == wall || m[adjRight.Y][adjRight.X] == wall {
+			// wall adjacent to one of the halves
+			return nil, nil
+		}
+		if m[adjLeft.Y][adjLeft.X] == boxLeft || m[adjLeft.Y][adjLeft.X] == boxRight || m[adjLeft.Y][adjLeft.X] == box {
+			return maybeMoveBox(m, *adjLeft, dir)
+		}
+		if m[adjRight.Y][adjRight.X] == boxRight || m[adjRight.Y][adjRight.X] == boxLeft || m[adjRight.Y][adjRight.X] == box {
+			return maybeMoveBox(m, *adjRight, dir)
+		}
+		// Neither adjacency contains a wall, box, or edge, so they are empty
+		m[posLeft.Y][posLeft.X], m[adjLeft.Y][adjLeft.X] = m[adjLeft.Y][adjLeft.X], m[posLeft.Y][posLeft.X]
+		m[posRight.Y][posRight.X], m[adjRight.Y][adjRight.X] = m[adjRight.Y][adjRight.X], m[posRight.Y][posRight.X]
+		return &pos, adjLeft
+	case advent.Left:
+		if adjLeft == nil {
+			return nil, nil
+		}
+		if m[adjLeft.Y][adjLeft.X] == wall {
+			return nil, nil
+		}
+		if m[adjLeft.Y][adjLeft.X] == boxRight || m[adjLeft.Y][adjLeft.X] == box {
+			return maybeMoveBox(m, *adjLeft, dir)
+		}
+		// move the left side first to not overwrite the left side of the box with the right side
+		// left moves over one to the adjacent spot; right swaps with the spot vacated by left
+		m[posLeft.Y][posLeft.X], m[adjLeft.Y][adjLeft.X] = m[adjLeft.Y][adjLeft.X], m[posLeft.Y][posLeft.X]
+		m[posRight.Y][posRight.X], m[posLeft.Y][posLeft.X] = m[posLeft.Y][posLeft.X], m[posRight.Y][posRight.X]
+		return &posLeft, adjLeft
+	case advent.Right:
+		if adjRight == nil {
+			return nil, nil
+		}
+		if m[adjRight.Y][adjRight.X] == wall {
+			return nil, nil
+		}
+		if m[adjRight.Y][adjRight.X] == boxLeft || m[adjRight.Y][adjRight.X] == box {
+			return maybeMoveBox(m, *adjRight, dir)
+		}
+		// move the right side into the adjacency; swap the left into the spot vacated by right
+		m[posRight.Y][posRight.X], m[adjRight.Y][adjRight.X] = m[adjRight.Y][adjRight.X], m[posRight.Y][posRight.X]
+		m[posLeft.Y][posLeft.X], m[posRight.Y][posRight.X] = m[posRight.Y][posRight.X], m[posLeft.Y][posLeft.X]
+		return &posLeft, &posRight
+	}
+
 	return nil, nil
 }
 
@@ -120,7 +245,7 @@ func score(m [][]object) int {
 	sum := 0
 	for y := range m {
 		for x := range m[y] {
-			if m[y][x] == box {
+			if m[y][x] == box || m[y][x] == boxLeft {
 				sum += 100*y + x
 			}
 		}
@@ -139,8 +264,8 @@ const (
 	robot
 )
 
-func mustParseInput() ([][]object, []advent.Direction) {
-	scanner := bufio.NewScanner(os.Stdin)
+func mustParseInput(r io.Reader) ([][]object, []advent.Direction) {
+	scanner := bufio.NewScanner(r)
 	m := mustParseMap(scanner)
 	dirs := mustParseDirs(scanner)
 	return m, dirs
---
+++ 2024/15/warehouse_test.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+func Test_debugPart2(t *testing.T) {
+
+	f, err := os.Open("/path/to/input")
+	require.NoError(t, err)
+	defer f.Close()
+
+	m, dirs := mustParseInput(f)
+	part2(m, dirs)
+}
+*/
