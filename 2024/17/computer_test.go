4e4aa4821e74e08cb802b42d4938ec02e97c1a27
Andrei Mackenzie <andreimackenzie@gmail.com>
2025-01-10 17:08:12 -0500

    comment out test only used for debugger

--- 2024/17/computer_test.go
+++ 2024/17/computer_test.go
@@ -1,12 +1,6 @@
 package main
 
-import (
-	"os"
-	"testing"
-
-	"github.com/stretchr/testify/require"
-)
-
+/*
 func Test_debug(t *testing.T) {
 	f, err := os.Open("/path/to/input.txt")
 	require.NoError(t, err)
@@ -15,3 +9,4 @@ func Test_debug(t *testing.T) {
 	c := mustParseInput(f)
 	part1(c)
 }
+*/
