96fd39ce8d1cac23dafe7e721abc43504115aa32
Andrei Mackenzie <andreimackenzie@gmail.com>
2025-01-20 14:50:55 -0500

    2024-12-19 - part 1 outline

---
+++ 2024/19/towel.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	/*
+		part 1:
+		- Sequences composed of single stripe colors that are available are easier to satisfy, so prioritize longer patterns
+		- For a given sequence, identify the longest towels that can fit.
+		- For each fitting towel, place it to create a matched subsequence & sub-sequences that require matches.
+		- For each subsequence, recursively attempt to identify the longest towel that can fit.
+		- A sequence is matched if a fitting towel is found & there are no sub-sequences, or every sub-sequence is recursively matched
+	*/
+}
