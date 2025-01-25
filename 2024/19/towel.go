f23d11e25e8541bf73bed3582e430f87b1a183e3
Andrei Mackenzie <andreimackenzie@gmail.com>
2025-01-25 17:24:19 -0500

    2024-12-19 - part 2
    The recursive condition could be simplified quite a bit by only
    processing the head of the pattern instead of trying to align each towel
    in each possible position.

--- 2024/19/towel.go
+++ 2024/19/towel.go
@@ -8,14 +8,9 @@ import (
 	"os"
 	"sort"
 	"strings"
-
-	"github.com/andrei-m/aoc/advent"
 )
 
-var debug = false
-
 func main() {
-	debug = advent.DebugEnabled()
 	/*
 		part 1:
 		- Sequences composed of single stripe colors that are available are easier to satisfy, so prioritize longer patterns
@@ -26,6 +21,7 @@ func main() {
 	*/
 	towels, patterns := mustParseInput(os.Stdin)
 	fmt.Printf("part 1: %d\n", part1(towels, patterns))
+	fmt.Printf("part 2: %d\n", part2(towels, patterns))
 }
 
 func part1(towels, patterns []string) int {
@@ -33,65 +29,54 @@ func part1(towels, patterns []string) int {
 		return len(towels[i]) > len(towels[j])
 	})
 
-	memo := map[string]bool{}
+	memo := map[string]int{}
 	possiblePatternCount := 0
 	for _, p := range patterns {
-		if isPossible(memo, towels, p) {
-			log.Printf("%s is possible", p)
+		if countPossibilities(memo, towels, p) > 0 {
+			log.Printf("possible: %s", p)
 			possiblePatternCount++
+		} else {
+			log.Printf("not possible: %s", p)
 		}
 	}
 	return possiblePatternCount
 }
 
-func isPossible(memo map[string]bool, towels []string, pattern string) bool {
-	possible, found := memo[pattern]
+func part2(towels, patterns []string) int {
+	sort.Slice(towels, func(i, j int) bool {
+		return len(towels[i]) > len(towels[j])
+	})
+
+	memo := map[string]int{}
+	totalPossibilities := 0
+	for _, p := range patterns {
+		c := countPossibilities(memo, towels, p)
+		log.Printf("%s: %d", p, c)
+		totalPossibilities += c
+	}
+	return totalPossibilities
+}
+
+func countPossibilities(memo map[string]int, towels []string, pattern string) int {
+	if len(pattern) == 0 {
+		// assume an empty pattern means exactly 1 way to satisfy a previous non-empty pattern
+		return 1
+	}
+
+	c, found := memo[pattern]
 	if found {
-		return possible
-	}
-	if debug {
-		log.Println(pattern)
+		return c
 	}
 
+	count := 0
 	for _, t := range towels {
-		for i := 0; i+len(t) <= len(pattern); i++ {
-			if t == pattern[i:i+len(t)] {
-				if debug {
-					log.Printf("%s matched at index %d", t, i)
-				}
-
-				if len(t) == len(pattern) {
-					// no remaining patterns to match on either end of the towel
-					memo[pattern] = true
-					return true
-				} else if i > 0 && i+len(t) < len(pattern) {
-					// remaining patterns exist on both ends
-					if isPossible(memo, towels, pattern[0:i]) && isPossible(memo, towels, pattern[i+len(t):]) {
-						memo[pattern] = true
-						return true
-					}
-				} else if i > 0 {
-					// remaining pattern exists the left only
-					if isPossible(memo, towels, pattern[0:i]) {
-						memo[pattern] = true
-						return true
-					}
-				} else {
-					// remaining pattern exists on the right only
-					if isPossible(memo, towels, pattern[i+len(t):]) {
-						memo[pattern] = true
-						return true
-					}
-				}
-			}
+		if len(t) <= len(pattern) && t == pattern[:len(t)] {
+			count += countPossibilities(memo, towels, pattern[len(t):])
 		}
 	}
 
-	if debug {
-		log.Printf("%s is not possible", pattern)
-	}
-	memo[pattern] = false
-	return false
+	memo[pattern] = count
+	return count
 }
 
 func mustParseInput(r io.Reader) ([]string, []string) {
