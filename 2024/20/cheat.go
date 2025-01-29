b7d2da019f03f456cd447aca8508ac0ef882d1d5
Andrei Mackenzie <andreimackenzie@gmail.com>
2025-01-28 19:24:33 -0500

    outline part 1

--- 2024/20/cheat.go
+++ 2024/20/cheat.go
@@ -11,7 +11,16 @@ import (
 
 func main() {
 	maze := mustParseInput(os.Stdin)
-	fmt.Printf("%v", maze)
+	yOverflow, xOverflow := len(maze), len(maze[0])
+	fmt.Printf("%dx%d grid", xOverflow, yOverflow)
+
+	/*
+		part 1:
+			disabling colisions still costs movement: 1 picosecond per d-pad move, including through what are normally walls.
+			1. Build a node adjacency graph for each unblocked D-pad direction starting from S
+			2. Run Djikstra's algorithm to find the shortest path from S to E. Each edge cost is 1. The number of picoseconds is the baseline
+			3. For each wall inside the perimeter on rows & columns with indices 0 and 140: Replace the wall with a path and re-run steps 1 and 2. If the shortest path is 100 or fewer than the baseline, count the wall as a cheat.
+	*/
 }
 
 type object int
